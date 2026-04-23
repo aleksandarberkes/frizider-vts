35fa05f9975c075601f7f24f26025c57c4baa0ea
Luka Dulac <lukadulac4@gmail.com>
2026-04-23 13:32:54 +0200

    feat: Refactor App component to use Layout and add routing for pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css';
-import Header from './components/header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './layout/Layout';
+import Favorites from './pages/Favorites';
+import Fridge from './pages/Fridge';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Recepti from './pages/Recepti';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recepti />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/fridge" element={<Fridge />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Route>
+    </Routes>
   );
 }
 
